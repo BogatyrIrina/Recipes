327137328be6714f6a5e84812f755ee49b98fbeb
admin <kochereva_irina@mail.ru>
2023-03-02 00:37:54 +0300

    задание выполнено частично

--- src/main/java/me/bogatyr/recipes/controllers/FirstController.java
+++ src/main/java/me/bogatyr/recipes/controllers/FirstController.java
@@ -1,2 +1,18 @@
-package me.bogatyr.recipes.controllers;public class FirstController {
+package me.bogatyr.recipes.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping
+    public String first() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        return "имя ученика";
+    }
 }
+
