f0fe758786363c64617a7e3584900697bd791f11
admin <kochereva_irina@mail.ru>
2023-03-12 21:56:19 +0300

    Библиотеки подключены

--- src/main/java/me/bogatyr/recipes/controllers/RecipeController.java
+++ src/main/java/me/bogatyr/recipes/controllers/RecipeController.java
@@ -1,5 +1,7 @@
 package me.bogatyr.recipes.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import me.bogatyr.recipes.dto.RecipeDTO;
 import me.bogatyr.recipes.model.Recipe;
 import me.bogatyr.recipes.service.RecipeService;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/recipe")
+@Tag(name = "Рецепты", description = "CRUD-операции и другие эндпоинты по работе с рецептами")
 
 public class RecipeController {
     private final RecipeService recipeService;
@@ -17,25 +20,39 @@ public class RecipeController {
     private RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
-     @GetMapping
-     public List<RecipeDTO> getRecipes(){
+
+    @GetMapping
+    @Operation(summary = "Получение всех рецептов",
+            description = "Можно получить список всех рецептов")
+    public List<RecipeDTO> getRecipes() {
         return recipeService.getAllRecipes();
-     }
+    }
 
     @GetMapping("/{id}")
-    public RecipeDTO getRecipe(@PathVariable("id") int id){
+    @Operation(summary = "Получение рецептов по id",
+            description = "Получить рецепт можно по id")
+    public RecipeDTO getRecipe(@PathVariable("id") int id) {
         return recipeService.getRecipe(id);
     }
+
     @PostMapping
-    public RecipeDTO addRecipe(@RequestBody Recipe recipe){
+    @Operation(summary = "Добавление рецептов",
+            description = "Добавить рецепт можно по id")
+    public RecipeDTO addRecipe(@RequestBody Recipe recipe) {
         return recipeService.addRecipe(recipe);
     }
+
     @PutMapping("/{id}")
-    public RecipeDTO editRecipe (@PathVariable ("id") int id, @RequestBody Recipe recipe){
+    @Operation(summary = "Редактирование рецептов по id",
+            description = "Отредактировать рецепт можно по id")
+    public RecipeDTO editRecipe(@PathVariable("id") int id, @RequestBody Recipe recipe) {
         return recipeService.updateRecipe(id, recipe);
     }
+
     @DeleteMapping("/{id}")
-    public RecipeDTO deleteRecipe(@PathVariable("id") int id){
+    @Operation(summary = "Удаление рецептов по id",
+            description = "Удалить рецепт можно по id")
+    public RecipeDTO deleteRecipe(@PathVariable("id") int id) {
         return recipeService.deleteById(id);
     }
 }
