5353225064402e8cae5c8ab3065de1aabb9ad19e
admin <kochereva_irina@mail.ru>
2023-03-17 23:21:48 +0300

    Д/з по теме "Потоки данных и файлы"

--- src/main/java/me/bogatyr/recipes/service/RecipeService.java
+++ src/main/java/me/bogatyr/recipes/service/RecipeService.java
@@ -1,9 +1,13 @@
 package me.bogatyr.recipes.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import me.bogatyr.recipes.dto.RecipeDTO;
 import me.bogatyr.recipes.model.Recipe;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,11 +15,18 @@ import java.util.Map;
 
 @Service
 public class RecipeService {
+    final private FilesService filesService;
     private int idCounter = 0;
     private final Map<Integer, Recipe> recipes = new HashMap<>();
+
+    public RecipeService(FilesService filesService) {
+        this.filesService = filesService;
+    }
+
     public RecipeDTO addRecipe(Recipe recipe){
         int id = idCounter++;
         recipes.put(id, recipe);
+        saveToFile();
         return RecipeDTO.from(id,recipe);
     }
     public RecipeDTO getRecipe(int id){
@@ -41,6 +52,7 @@ public class RecipeService {
             throw new RecipeNotFoundException();
         }
         recipes.put(id, recipe);
+        saveToFile();
         return RecipeDTO.from(id, recipe);
     }
 
@@ -50,5 +62,26 @@ public class RecipeService {
             throw new RecipeNotFoundException();
         }
         return RecipeDTO.from(id, existingRecipe);
+    }
+    private void saveToFile(){
+        try {
+            String json = new ObjectMapper().writeValueAsString(recipes);
+            filesService.saveToFile(json);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void readFromFile(){
+        String json = filesService.readFromFile();
+        try {
+            new ObjectMapper().readValue(json, new TypeReference<HashMap<Integer, Recipe>>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @PostConstruct
+    private void init(){
+        readFromFile();
     }
 }
